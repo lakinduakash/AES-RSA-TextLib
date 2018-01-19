c4df9c016286a89d9fe2460d1b1414febcc1a21c
lakinduakash <lakinduakash@gmail.com>
2018-01-19 12:11:44 +0530

    Adding copyright notice

--- src/AES.java
+++ src/AES.java
@@ -1,9 +1,26 @@
+/*
+Copyright 2018 Lakindu Akash
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import javax.crypto.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 /**
+ * @author lakinduakash
  * Created by lakinduakash on 1/17/18.
  */
 public class AES
@@ -84,7 +101,7 @@ public class AES
     public static String getDecryptedDataBase64(SecretKey key,byte[] data) throws BadPaddingException,IllegalBlockSizeException,
             InvalidKeyException
     {
-            return Base64.getEncoder().encodeToString(decrypt(key,data));
+        return Base64.getEncoder().encodeToString(decrypt(key,data));
     }
 
     public static String getDecryptedDataBase64(SecretKey key,String base64EncodeString) throws BadPaddingException,IllegalBlockSizeException,
@@ -92,7 +109,6 @@ public class AES
     {
         return Base64.getEncoder().encodeToString(decrypt(key,Base64.getDecoder().decode(base64EncodeString)));
     }
-
 
 
     public static String getAlgorithm()
