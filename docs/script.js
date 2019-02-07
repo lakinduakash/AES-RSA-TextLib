c073567945776603818b8827dbc967ffef1fc604
Lakindu Akash <Lakinduakash@gmail.com>
2019-02-07 18:48:34 +0530

    update doc

--- docs/script.js
+++ docs/script.js
@@ -12,6 +12,20 @@ function show(type) {
     updateTabs(type);
 }
 
+function masking(type) {
+    count = 0;
+    for (var key in methods) {
+        var row = document.getElementById(key);
+        if ((methods[key] & type) != 0) {
+            row.style.display = '';
+            row.className = (count++ % 2) ? rowColor : altColor;
+        }
+        else
+            row.style.display = 'none';
+    }
+    updateTabs(type);
+}
+
 function updateTabs(type) {
     for (var value in tabs) {
         var sNode = document.getElementById(tabs[value][0]);
